3dff0ed34f2574b7a21566175bde605f40df7cb4
alfianlion <alfianlion@gmail.com>
2021-11-16 12:43:45 +0800

    Added base change function for url to work on development / production

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+// URL
+/*
+    Javascript to set the base to which url was being directed. 
+    VALUES TO CHANGE WHEN PUSHING TO GITHUB
+
+    False = Github Pages URL
+    True  = localhost
+*/
+
+baseChange(true)
+
+function baseChange(bool){
+    if (bool == false){
+        // For Localhost
+        var base = document.createElement('base');
+        base.href = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+        document.getElementsByTagName('head')[0].appendChild(base);
+        alert("Current Page is on Localhost base!\nDo change to production base if necessary.");
+    } else{
+        // For Github Pages
+        var base = document.createElement('base');
+        base.href = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + "/My-Portfolio/";
+        document.getElementsByTagName('head')[0].appendChild(base);
+        alert("Current Page is on Github base!\nDo change to development base if necessary.");
+    }
+}
+
+
+
