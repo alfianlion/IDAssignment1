bdd3b857cabdab39a573b60c065f29437feaf348
alfianlion <alfianlion@gmail.com>
2021-12-03 10:08:10 +0800

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 innit();
 
-baseChange(true);
+// baseChange(true);
 
 document.getElementById("colored").addEventListener('click',function(){
 
@@ -83,27 +83,27 @@ function innit(){
     }
 }
 
-function baseChange(bool){
+// function baseChange(bool){
 
-    /*
-        baseChange() is a function to set the base to which url was being directed. 
-        VALUES TO CHANGE WHEN PUSHING TO GITHUB
+//     /*
+//         baseChange() is a function to set the base to which url was being directed. 
+//         VALUES TO CHANGE WHEN PUSHING TO GITHUB
 
-        True  = Github Pages URL
-        False = localhost
-    */
+//         True  = Github Pages URL
+//         False = localhost
+//     */
    
-    if (bool == false){
-        // For Localhost
-        var base = document.createElement('base');
-        base.href = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
-        document.getElementsByTagName('head')[0].appendChild(base);
-        // alert("Current Page is on Localhost base!\nDo change to production base if necessary.");
-    } else{
-        // For Github Pages
-        var base = document.createElement('base');
-        base.href = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + "/IDAssignment1";
-        document.getElementsByTagName('head')[0].appendChild(base);
-        // alert("Current Page is on Github base!\nDo change to development base if necessary.");
-    }
-}
+//     if (bool == false){
+//         // For Localhost
+//         var base = document.createElement('base');
+//         base.href = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+//         document.getElementsByTagName('head')[0].appendChild(base);
+//         // alert("Current Page is on Localhost base!\nDo change to production base if necessary.");
+//     } else{
+//         // For Github Pages
+//         var base = document.createElement('base');
+//         base.href = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + "/IDAssignment1";
+//         document.getElementsByTagName('head')[0].appendChild(base);
+//         // alert("Current Page is on Github base!\nDo change to development base if necessary.");
+//     }
+// }
