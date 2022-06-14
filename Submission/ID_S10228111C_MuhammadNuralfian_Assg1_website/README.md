e8025216bbbd89c91030141a44163d645494df3d
alfianlion <alfianlion@gmail.com>
2022-06-14 12:35:24 +0800

    Submission Completed

---
+++ Submission/ID_S10228111C_MuhammadNuralfian_Assg1_website/README.md
@@ -0,0 +1,81 @@
+# Nuralfian Portfolio
+
+This project is to showcase my portfolio in a **HTML,CSS,JS** format. Included my _proudest projects_ made during my career as a web developer and to get to know _about myself **more**_!
+
+![First Image](/images/first_image_website.PNG)
+![Second Image](/images/second_image_website.PNG)
+
+## Design Process
+
+The approach to the design of the website is to keep it minimal as possible. A **_minilistic look_** of course.
+
+Having the thought in mind, I would like to make the website as interactive as possible with the Color Version Switcher for the website.
+
+The desired goal for the users to achieve is to better understand me and the skills I have as it is a portfolio of myself.
+
+Wireframe: To access, Click [Link here](https://xd.adobe.com/view/2586de42-9875-4f4a-9bc1-c48c4c2e5971-70d5/)
+
+Document: To access, Click [Link here](https://docs.google.com/document/d/1HLVK-dt6wwK-BEuL8pES8D6vaaSgA3CsKXcMYcIqkfQ/edit?usp=sharing)
+
+## Features
+
+### Existing Features
+
+- Color version switching - Allows user to change preferred style of viewing the website: **_Colored / Uncolored_**
+  - Color version switching keeps the user input and reflect CSS to every page in the website
+- Animation - When scalling browser on laptop, it will _dynamically scale smoothly_
+- MailTo - Clicking on the email icon in _contact page_ allows user to email me with executing mail app in the user device.
+- PhoneTo - Clicking on the phone icon in _contact page_ allows user to call me via the phone.
+
+### Features Left to Implement
+
+- Enable font-size adjust based on user preferences
+  - Increase accessibility of the website
+- Light/Dark Mode for preferences
+
+## Technologies Used
+
+- HTML
+  - HyperText Markup Language (HTML) to **display** elements in a **web browser**
+- CSS
+  - Cascading Style Sheet (CSS) to stylise the elements in HTML to make HTML elements **presentable**
+- Javascript
+  - Javascript(JS) allows **interactivity** between HTML elements and can change CSS on elements with **functions / logics**
+
+## Testing
+
+I. On every page:
+
+1. Scroll down to the footer
+2. On the left of the footer, Click on 'Colored Ver'
+   - Is a button to the colored version of the website
+3. Website now has colors and background images
+
+## Credits
+
+### Content
+
+- The text for Project 1 in Project page is from the main [Interviewsconnect website](https://www.interviewsconnect.com/)
+- The text for Project 2 in Project page is from the main [Jigsaw Me website](https://jigsawme.antgility.com/)
+
+### Media
+
+- Main Background for the top image is from [Picsum](https://picsum.photos/seed/picsum/2000/2000)
+- Project one image is from [Interviewsconnect website](https://www.interviewsconnect.com/)
+- Project two image is from [Jigsaw Me website](https://jigsawme.antgility.com/)
+- Gmail logo is from [Gmail](https://www.google.com/intl/en-GB/gmail/about/static-2.0/images/logo-gmail.png?fingerprint=c2eaf4aae389c3f885e97081bb197b97)
+- LinkedIn logo is from [LinkedIn](https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg)
+- Phone image is from [pngkey.com](https://pngkey.com/)
+
+### Acknowledgment
+
+- [Markdown Template](https://github.com/immalcolm/interactivedev-readme-template) - Making README.MD organised and clear
+- [Patrick David](https://bepatrickdavid.com/) - Inspiring to try our the minimalistic look + Colored version mode
+- [Valorant](https://playvalorant.com/en-sg/) - Inspiring to try out the font stroke in the background
+
+### Author
+
+- Created for **Practical Assignment** ~~Personal Use~~
+- Done by:
+  - Name: Muhmmad Nuralfian
+  - Student ID: S10228111C
---
+++ Submission/ID_S10228111C_MuhammadNuralfian_Assg1_website/scripts/main.js
@@ -0,0 +1,127 @@
+innit();
+
+baseChange(false);
+
+document.getElementById("colored").addEventListener('click',function(){
+
+    /*  
+        This EventListener add as a event handler to a DOM. 
+        This will store user input as to which version user prefers to view the website
+    */
+
+    let toggled = sessionStorage.getItem("colored");
+    if (toggled === "false" || toggled === ""){
+        sessionStorage.setItem('colored',"true");
+        toggled = sessionStorage.getItem("colored");
+        colorver(toggled);
+    } else {
+        sessionStorage.setItem('colored',"false");
+        toggled = sessionStorage.getItem("colored");
+        colorver(toggled);
+    }
+})
+
+
+function colorver(toggle) {
+
+    /*
+        function colorver() is a toggle function for the text 'Colored Ver' / 'Uncolored Ver'
+        When clicked, it will change all the <span> elements to the desired color / uncolored ver.
+        Additionally, the background will be display or not depending on it as well.
+
+        True    = Colored ver 
+        False   = Uncolored ver
+
+    */
+
+    // All span element
+    let span = document.querySelectorAll("span.darkyellow");
+
+        if (toggle === "false"){
+            for (let i = 0; i < span.length; i++) {
+                document.getElementById("colored").innerHTML = "Colored Ver";        
+                document.getElementById("colored").style.color = 'white';        
+                span[i].style.color = 'white';
+            }        
+        } else {
+            for (let i = 0; i < span.length; i++) {
+                document.getElementById("colored").innerHTML = "Uncolored Ver";
+                document.getElementById("colored").style.color = '#c28e2c';        
+                span[i].style.color = '#c28e2c';
+            }    
+        }
+    
+    // background image
+    let bg = document.querySelector("main");
+
+        if (toggle === "false"){
+            bg.style.backgroundImage = '';
+        } else {
+            bg.style.backgroundImage = "url('https://picsum.photos/seed/picsum/2000/2000')";
+        }
+
+ } 
+
+function innit(){
+
+    /*
+    function innit() initialize the color of the element in the page.
+    Colors and text change based on what user clicked on 'Colored Ver' / 'Uncolored Ver'.
+    Using sessionStorage to store user input.
+
+    True    = Colored ver 
+    False   = Uncolored ver
+*/
+
+    let toggled = sessionStorage.getItem("colored");
+    if (toggled === "false" || toggled === ""){
+        colorver(toggled);
+        document.getElementById("colored").innerHTML = "Colored Ver" 
+    } else {
+        colorver(toggled);
+        document.getElementById("colored").innerHTML = "Uncolored Ver" 
+    }
+}
+
+function baseChange(bool){
+
+    /*
+        baseChange() is a function to set the base to which url was being directed. 
+        VALUES TO CHANGE WHEN PUSHING TO GITHUB
+
+        True  = Github Pages URL
+        False = localhost
+    */
+   
+    if (bool == false){
+        // For Localhost
+        let home = document.getElementById("hpage")
+        home.addEventListener('click',function(){
+            home.location.href = "/index.html";
+        })
+        let project = document.getElementById("ppage")
+        project.addEventListener('click',function(){
+            project.location.href = "/project.html";
+        })
+        let contact = document.getElementById("cpage")
+        contact.addEventListener('click',function(){
+            contact.location.href = "/contact.html";
+        })
+        // alert("Current Page is on Localhost base!\nDo change to production base if necessary.");
+    } else{
+        // For Github Pages
+        let home = document.getElementById("hpage")
+        home.addEventListener('click',function(){
+            home.href = "/IDAssignment1/index.html";
+        })
+        let project = document.getElementById("ppage")
+        project.addEventListener('click',function(){
+            project.href = "/IDAssignment1/project.html";
+        })
+        let contact = document.getElementById("cpage")
+        contact.addEventListener('click',function(){
+            contact.href = "/IDAssignment1/contact.html";
+        })
+        // alert("Current Page is on Github base!\nDo change to development base if necessary.");
+    }
+}
